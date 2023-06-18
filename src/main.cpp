478e9c8ecff56788e4b273170383ad8fa9ead9c1
SamTheDev <vs.samu.dev@gmail.com>
2023-06-18 18:13:36 +0200

    updated music playing

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <cstdio>
 #include <dpp/dpp.h>
 // #include <fmt/format.h>
+#include <dpp/presence.h>
 #include <iomanip>
 // #include <nlohmann/json.hpp>
 #include <sstream>
 
+#include <dirent.h>
 #include <fstream>
 #include <iostream>
 #include <mpg123.h>
@@ -26,8 +28,40 @@ std::string getToken(std::string path) {
     return token;
 }
 
+void scanForFiles(std::vector<std::string> &files) {
+    files.clear();
+
+    // List all Music Files
+    DIR *dir;
+    struct dirent *ent;
+    if ((dir = opendir("audio/")) != NULL) {
+        /* print all the files and directories within directory */
+        printf("Audio Files:\n");
+        while ((ent = readdir(dir)) != NULL) {
+            if (ent->d_type == DT_REG) {
+                const char *ext = strrchr(ent->d_name, '.');
+                if (!strcmp(ext, ".mp3")) {
+                    files.push_back(ent->d_name);
+                    printf("%s\n", ent->d_name);
+                }
+            }
+        }
+        printf("----\n");
+        closedir(dir);
+    } else {
+        /* could not open directory */
+        perror("");
+        exit(EXIT_FAILURE);
+    }
+}
+
 /* For an example we will hardcode a path to some awesome music here */
 int main(int argc, char const *argv[]) {
+    std::vector<std::string> files;
+    srand(time(0));
+
+    scanForFiles(files);
+
     // RAW PCM: data, 2 channel stereo, 16 bit signed 48000Hz.
     std::vector<uint8_t> pcmdata;
 
@@ -35,10 +69,11 @@ int main(int argc, char const *argv[]) {
     dpp::cluster bot(getToken("../TOKEN"),
                      dpp::i_default_intents | dpp::i_message_content);
 
-    // bot.on_log(dpp::utility::cout_logger());
+    bot.on_log(dpp::utility::cout_logger());
 
     // onMessage()
-    bot.on_message_create([&bot, &pcmdata](const dpp::message_create_t &event) {
+    bot.on_message_create([&bot, &pcmdata,
+                           &files](const dpp::message_create_t &event) {
         char cmd[64], data[256];
         sscanf(event.msg.content.c_str(), "%s %[^\n]", cmd, data);
 
@@ -52,62 +87,95 @@ int main(int argc, char const *argv[]) {
             }
         }
 
+        // Get Music
+        if (!strcmp(cmd, ".get")) {
+            // Download Music
+            std::string url =
+                //"https://www.youtube.com/watch?v=" +
+                std::string(data);
+
+            system(("cd audio && yt-dlp -x --audio-format mp3 " + url).c_str());
+        }
+
+        // List Musics
+        if (!strcmp(cmd, ".list")) {
+            scanForFiles(files);
+
+            if (!strcmp(data, "number"))
+                event.reply("There are " + std::to_string(files.size()) +
+                            " Audio Files Available");
+            else {
+                std::string list;
+
+                for (auto f : files)
+                    list += f + "\n";
+
+                event.reply("Available Audio Files:\n" + list);
+            }
+        }
+
         // Play Music from YouTube
         if (!strcmp(cmd, ".play")) {
-            // Download Music
-            std::string url = "yt-dlp -x --audio-format mp3 "
-                              "https://www.youtube.com/watch?v=" +
-                              std::string(data) + " -o audio.mp3";
+            uint id = 0;
+            int n = sscanf(data, "%i", &id);
 
-            system("rm -rf audio.mp3");
-            system(url.c_str());
+            if (id >= files.size())
+                event.reply("ID out of index!\n");
+            else {
+                if (n != 1)
+                    id = rand() % files.size();
 
-            // Load Music
-            pcmdata.clear();
-            mpg123_init();
+                event.reply("Playing: " + files[id]);
 
-            int err = 0;
-            unsigned char *buffer;
-            size_t buffer_size, done;
-            int channels, encoding;
-            long rate;
+                // Load Music
+                pcmdata.clear();
+                mpg123_init();
 
-            /* Note it is important to force the frequency to 48000 for Discord
-             * compatibility */
-            mpg123_handle *mh = mpg123_new(NULL, &err);
-            mpg123_param(mh, MPG123_FORCE_RATE, 48000, 48000.0);
+                int err = 0;
+                unsigned char *buffer;
+                size_t buffer_size, done;
+                int channels, encoding;
+                long rate;
 
-            /* Decode entire file into a vector. You could do this on the fly,
-             * but if you do that you may get timing issues if your CPU is busy
-             * at the time and you are streaming to a lot of channels/guilds.
-             */
-            buffer_size = mpg123_outblock(mh);
-            buffer = new unsigned char[buffer_size];
+                /* Note it is important to force the frequency to 48000 for
+                 * Discord compatibility */
+                mpg123_handle *mh = mpg123_new(NULL, &err);
+                mpg123_param(mh, MPG123_FORCE_RATE, 48000, 48000.0);
 
-            /* Note: In a real world bot, this should have some error logging */
-            mpg123_open(mh, "audio.mp3");
-            mpg123_getformat(mh, &rate, &channels, &encoding);
+                /* Decode entire file into a vector. You could do this on the
+                 * fly, but if you do that you may get timing issues if your CPU
+                 * is busy at the time and you are streaming to a lot of
+                 * channels/guilds.
+                 */
+                buffer_size = mpg123_outblock(mh);
+                buffer = new unsigned char[buffer_size];
 
-            unsigned int counter = 0;
-            for (int totalBytes = 0;
-                 mpg123_read(mh, buffer, buffer_size, &done) == MPG123_OK;) {
-                for (auto i = 0; i < buffer_size; i++) {
-                    pcmdata.push_back(buffer[i]);
+                /* Note: In a real world bot, this should have some error
+                 * logging */
+                mpg123_open(mh, ("audio/" + files[id]).c_str());
+                mpg123_getformat(mh, &rate, &channels, &encoding);
+
+                unsigned int counter = 0;
+                for (int totalBytes = 0; mpg123_read(mh, buffer, buffer_size,
+                                                     &done) == MPG123_OK;) {
+                    for (auto i = 0; i < buffer_size; i++) {
+                        pcmdata.push_back(buffer[i]);
+                    }
+                    counter += buffer_size;
+                    totalBytes += done;
                 }
-                counter += buffer_size;
-                totalBytes += done;
-            }
-            delete buffer;
-            mpg123_close(mh);
-            mpg123_delete(mh);
+                delete buffer;
+                mpg123_close(mh);
+                mpg123_delete(mh);
 
-            // Play
-            dpp::voiceconn *v = event.from->get_voice(event.msg.guild_id);
-            if (v && v->voiceclient && v->voiceclient->is_ready()) {
-                /* Stream the already decoded MP3 file. This passes the PCM data
-                 * to the library to be encoded to OPUS */
-                v->voiceclient->send_audio_raw((uint16_t *)pcmdata.data(),
-                                               pcmdata.size());
+                // Play
+                dpp::voiceconn *v = event.from->get_voice(event.msg.guild_id);
+                if (v && v->voiceclient && v->voiceclient->is_ready()) {
+                    /* Stream the already decoded MP3 file. This passes the PCM
+                     * data to the library to be encoded to OPUS */
+                    v->voiceclient->send_audio_raw((uint16_t *)pcmdata.data(),
+                                                   pcmdata.size());
+                }
             }
         }
     });
